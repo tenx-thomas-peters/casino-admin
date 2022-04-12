b3769db46ad649f6b6a5d3a92b90b0842e1a8a0c
Thomas Peters <pianist.chopin.1@gmail.com>
2022-04-12 16:53:57 -0400

    fix money charge without calling api when charge in admin manually

--- src/main/java/com/casino/modules/admin/controller/MemberController.java
+++ src/main/java/com/casino/modules/admin/controller/MemberController.java
@@ -386,26 +386,6 @@ public class MemberController {
             String reasonStrKey = reasonList.get(0).getStrValue();
             reason = messageSource.getMessage(reasonStrKey, null, Locale.ENGLISH);
 
-            /* Thomas Peters 2022.04.06 --------------------------------
-             * Change money amount of member in admin member management page manually ------------------ <
-             */
-            ResponseEntity<String> ret;
-            Member member = memberService.getById(memberSeq);
-            if(classification == 0 && transactionClassification == 0){
-                ret = HttpUtils.userAddBalance(gameServerUrl + "/user/add-balance", member.getName(), variableAmount, apiKey);
-            }
-            else{
-                ret = HttpUtils.userSubBalance(gameServerUrl + "/user/sub-balance", member.getName(), variableAmount, apiKey);
-            }
-
-            if (ret.getStatusCode().value() == 200) {
-
-            }
-            else {
-                result.error505("sub -balance api failed");
-            }
-            // Change money amount of member in admin member management page manually ------------------ />
-
             float actualAmount = variableAmount;
             Float finalAmount = transactionClassification.equals(CommonConstant.MONEY_OPERATION_TYPE_DEPOSIT)
                     ? prevMoneyAmount + variableAmount
