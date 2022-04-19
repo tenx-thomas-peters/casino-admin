534e9461b8d136be6245773efbf84aead1e6b979
Thomas Peters <pianist.chopin.1@gmail.com>
2022-04-19 19:59:54 -0400

    scheduleService

--- src/main/java/com/casino/modules/admin/service/impl/ScheduleServiceImpl.java
+++ src/main/java/com/casino/modules/admin/service/impl/ScheduleServiceImpl.java
@@ -153,38 +153,120 @@ public class ScheduleServiceImpl extends ServiceImpl<ScheduleMapper, BettingSumm
 
                     float slot_distributor_rate_amount = 0;
                     float baccarat_distributor_rate_amount = 0;
-                    float slot_headquarter_rate_amount = 0;
-                    float baccarat_headquarter_rate_amount = 0;
 
+                    //-------- get store
                     if(member.getStoreSeq() !=null && member.getStoreSeq().equals("")){
                         Member store_member = memberService.getById(member.getStoreSeq());
                         slot_store_rolling_amount = this.calulateRate(slotBettingAmount, store_member.getSlotRate());
                         baccarat_store_rolling_amount = this.calulateRate(baccaratBettingAmount, store_member.getBaccaratRate());
+
+                        float store_variableAmount = slot_store_rolling_amount + baccarat_store_rolling_amount;
+                        memberService.updateMemberHoldingMoney(
+                                store_member.getSeq(),
+                                store_member.getMoneyAmount(),
+                                0f,
+                                store_variableAmount,
+                                Math.abs(store_variableAmount),
+                                store_member.getMoneyAmount() + store_variableAmount,
+                                0,
+                                0,
+                                CommonConstant.MONEY_HISTORY_STATUS_PARTNER_PAYMENT,
+                                2,
+                                "store ",
+                                0
+                        );
                     }
 
+                    //-------- get distributor
                     if(member.getDistributorSeq() !=null && member.getDistributorSeq().equals("")){
                         Member distributor_member = memberService.getById(member.getDistributorSeq());
                         slot_distributor_rate_amount = this.calulateRate(slotBettingAmount, distributor_member.getSlotRate());
                         baccarat_distributor_rate_amount =  this.calulateRate(baccaratBettingAmount, distributor_member.getBaccaratRate());
-                    }
 
-                    if(member.getSubHeadquarterSeq() !=null && member.getSubHeadquarterSeq().equals("")){
-                        Member headquarter_member = memberService.getById(member.getSubHeadquarterSeq());
-                        slot_headquarter_rate_amount = this.calulateRate(slotBettingAmount, headquarter_member.getSlotRate());
-                        baccarat_headquarter_rate_amount = this.calulateRate(baccaratBettingAmount, headquarter_member.getBaccaratRate());
-                    }
-
-                    if(slot_headquarter_rate_amount > 0){
-                        slot_headquarter_rolling_amount = slot_headquarter_rate_amount - slot_distributor_rate_amount;
-                        baccarat_headquarter_rolling_amount = baccarat_headquarter_rate_amount - baccarat_distributor_rate_amount;
-                    }
-
-                    if (slot_distributor_rate_amount > 0) {
                         slot_distributor_rolling_amount = slot_distributor_rate_amount - slot_store_rolling_amount;
                         baccarat_distributor_rolling_amount = baccarat_distributor_rate_amount - baccarat_store_rolling_amount;
+
+                        float distributor_variableAmount = slot_distributor_rolling_amount + baccarat_distributor_rolling_amount;
+                        memberService.updateMemberHoldingMoney(
+                                distributor_member.getSeq(),
+                                distributor_member.getMoneyAmount(),
+                                0f,
+                                distributor_variableAmount,
+                                Math.abs(distributor_variableAmount),
+                                distributor_member.getMoneyAmount() + distributor_variableAmount,
+                                0,
+                                0,
+                                CommonConstant.MONEY_HISTORY_STATUS_PARTNER_PAYMENT,
+                                2,
+                                "partner",
+                                0
+                        );
                     }
 
+                    //-------- get headquarter
+                    if(member.getSubHeadquarterSeq() !=null && member.getSubHeadquarterSeq().equals("")){
+                        Member headquarter_member = memberService.getById(member.getSubHeadquarterSeq());
+                        slot_headquarter_rolling_amount = this.calulateRate(slotBettingAmount, headquarter_member.getSlotRate()) - slot_distributor_rate_amount;
+                        baccarat_headquarter_rolling_amount = this.calulateRate(baccaratBettingAmount, headquarter_member.getBaccaratRate()) - baccarat_distributor_rate_amount;
 
+                        float headquarter_variableAmount = slot_headquarter_rolling_amount + baccarat_headquarter_rolling_amount;
+                        memberService.updateMemberHoldingMoney(
+                                headquarter_member.getSeq(),
+                                headquarter_member.getMoneyAmount(),
+                                0f,
+                                headquarter_variableAmount,
+                                Math.abs(headquarter_variableAmount),
+                                headquarter_member.getMoneyAmount() + headquarter_variableAmount,
+                                0,
+                                0,
+                                CommonConstant.MONEY_HISTORY_STATUS_PARTNER_PAYMENT,
+                                2,
+                                "Headquarter",
+                                0
+                        );
+                    }
+
+                    //-------- get member
+                    float variableAmount = (slotWinningAmount + baccaratWinningAmount) - (slotBettingAmount+baccaratBettingAmount);
+
+                    // set Reason of money transfer--------------------------------------- <
+                    Integer reasonType = 0;
+
+                    QueryWrapper<Dict> qwe = new QueryWrapper<>();
+                    qwe.eq("dict_key", CommonConstant.DICT_KEY_MONEY_REASON);
+
+                    if((slotWinningAmount + baccaratWinningAmount) < (slotBettingAmount + baccaratBettingAmount)){
+                        qwe.eq("dict_value", CommonConstant.MONEY_REASON_TRANSFER);
+                        reasonType = CommonConstant.MONEY_REASON_TRANSFER;
+                    }
+                    else{
+                        qwe.eq("dict_value", CommonConstant.MONEY_REASON_TRANSFER_WINNING);
+                        reasonType = CommonConstant.MONEY_REASON_TRANSFER_WINNING;
+                    }
+
+                    List<Dict> reasonList = dictService.list(qwe);
+                    List<String> params = new ArrayList<String>();
+                    params.add(String.valueOf(variableAmount));
+                    String reason = messageSource.getMessage(reasonList.get(0).getStrValue(), params.toArray(), Locale.ENGLISH);
+                    // set Reason of money transfer--------------------------------------- />
+
+                    memberService.updateMemberHoldingMoney(
+                            member.getSeq(),
+                            member.getMoneyAmount(),
+                            0f,
+                            variableAmount,
+                            Math.abs(variableAmount),
+                            member.getMoneyAmount() + variableAmount,
+                            0,
+                            0,
+                            CommonConstant.MONEY_HISTORY_STATUS_PARTNER_PAYMENT,
+                            reasonType,
+                            reason,
+                            0
+                    );
+
+
+                    // save betting summary-----------------------------------------------------------------------------
                     bettingSummary.setSlotBettingAmount(slotBettingAmount);
                     bettingSummary.setBaccaratBettingAmount(baccaratBettingAmount);
                     bettingSummary.setSlotWinningAmount(slotWinningAmount);
@@ -200,70 +282,17 @@ public class ScheduleServiceImpl extends ServiceImpl<ScheduleMapper, BettingSumm
                     bettingSummary.setStoreSeq(member.getStoreSeq() != null ? member.getStoreSeq() : "");
                     bettingSummary.setDistributorSeq(member.getDistributorSeq() != null ? member.getDistributorSeq() : "");
                     bettingSummary.setHeadquarterSeq(member.getSubHeadquarterSeq() != null ? member.getSubHeadquarterSeq() : "");
-
                     bettingSummary.setSlotStoreRollingAmount(slot_store_rolling_amount);
                     bettingSummary.setBaccaratStoreRollingAmount(baccarat_store_rolling_amount);
                     bettingSummary.setSlotDistributorRollingAmount(slot_distributor_rolling_amount);
                     bettingSummary.setBaccaratDistributorRollingAmount(baccarat_distributor_rolling_amount);
                     bettingSummary.setSlotHeadquarterRollingAmount(slot_headquarter_rolling_amount);
                     bettingSummary.setBaccaratHeadquarterRollingAmount(baccarat_headquarter_rolling_amount);
+                    // save betting summary-----------------------------------------------------------------------------
+
                     // total amount of member
 
 //                    moneyHistory.setChargeCount(moneyHistory.getChargeCount() + 1);
-
-                    String memberSeq = member.getSeq();
-                    float prevMoneyAmount = member.getMoneyAmount();
-                    float prevMileageAmount = 0;
-                    float variableAmount = (slotWinningAmount + baccaratWinningAmount) - (slotBettingAmount+baccaratBettingAmount);
-                    float actualAmount = Math.abs(variableAmount);
-                    float finalAmount = member.getMoneyAmount() + variableAmount;
-                    Integer classification = 0;
-                    Integer transactionClassification = 0;
-                    Integer status = CommonConstant.MONEY_HISTORY_STATUS_PARTNER_PAYMENT;
-
-                    // set Reason of money transfer--------------------------------------- <
-                    Integer reasonType = 0;
-                    QueryWrapper<Dict> qwe = new QueryWrapper<>();
-                    qwe.eq("dict_key", CommonConstant.DICT_KEY_MONEY_REASON);
-
-                    if((slotWinningAmount + baccaratWinningAmount) < (slotBettingAmount+baccaratBettingAmount)){
-                        qwe.eq("dict_value", CommonConstant.MONEY_REASON_TRANSFER);
-                        reasonType = CommonConstant.MONEY_REASON_TRANSFER;
-                    }
-                    else{
-                        qwe.eq("dict_value", CommonConstant.MONEY_REASON_TRANSFER_WINNING);
-                        reasonType = CommonConstant.MONEY_REASON_TRANSFER_WINNING;
-                    }
-
-                    String reasonStrKey = "";
-                    List<Dict> reasonList = dictService.list(qwe);
-                    reasonStrKey = reasonList.get(0).getStrValue();
-                    List<String> params = new ArrayList<String>();
-                    params.add(String.valueOf(variableAmount));
-
-                    String reason = messageSource.getMessage(reasonStrKey, params.toArray(), Locale.ENGLISH);
-                    // set Reason of money transfer--------------------------------------- />
-
-                    Integer chargeCount = 0;
-                    if (memberService.updateMemberHoldingMoney(
-                            memberSeq,
-                            prevMoneyAmount,
-                            prevMileageAmount,
-                            variableAmount,
-                            actualAmount,
-                            finalAmount,
-                            classification,
-                            transactionClassification,
-                            status,
-                            reasonType,
-                            reason,
-                            chargeCount
-                    )){
-                        result = true;
-
-                    } else {
-                        result = false;
-                    }
 
                     bettingSummaryList.add(bettingSummary);
                 }
