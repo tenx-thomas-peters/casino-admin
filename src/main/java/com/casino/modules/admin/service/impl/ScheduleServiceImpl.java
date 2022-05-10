19ced6830a04ff5282f7f1e9c6baabaa02ce2314
Thomas Peters <pianist.chopin.1@gmail.com>
2022-05-09 21:49:33 -0400

    set slotrolling amount for member

--- src/main/java/com/casino/modules/admin/service/impl/ScheduleServiceImpl.java
+++ src/main/java/com/casino/modules/admin/service/impl/ScheduleServiceImpl.java
@@ -117,6 +117,8 @@ public class ScheduleServiceImpl extends ServiceImpl<ScheduleMapper, BettingSumm
 
                     totalBettingAmount.setLostAmount();
 
+                    float slot_rolling_amount = 0;
+                    float baccarat_rolling_amount = 0;
                     float slot_store_rolling_amount = 0;
                     float baccarat_store_rolling_amount = 0;
                     float slot_distributor_rolling_amount = 0;
@@ -127,15 +129,24 @@ public class ScheduleServiceImpl extends ServiceImpl<ScheduleMapper, BettingSumm
                     float slot_distributor_rate_amount = 0;
                     float baccarat_distributor_rate_amount = 0;
 
+                    float slot_store_rate_amount = 0;
+                    float baccarat_store_rate_amount = 0;
+
+                    slot_rolling_amount = this.calulateRate(totalBettingAmount.slotBettingAmount, member.getSlotRate());
+                    baccarat_rolling_amount = this.calulateRate(totalBettingAmount.baccaratBettingAmount, member.getBaccaratRate());
+
+
                     //-------- get store
                     if(member.getStoreSeq() !=null && !member.getStoreSeq().equals("")){
 
                         Member store_member = memberService.getById(member.getStoreSeq());
                         System.out.println("IScheduleService==saveBettingSummary========store_member:" + store_member);
 
-                        slot_store_rolling_amount = this.calulateRate(totalBettingAmount.slotBettingAmount, store_member.getSlotRate());
-                        baccarat_store_rolling_amount = this.calulateRate(totalBettingAmount.baccaratBettingAmount, store_member.getBaccaratRate());
+                        slot_store_rate_amount = this.calulateRate(totalBettingAmount.slotBettingAmount, store_member.getSlotRate());
+                        baccarat_store_rate_amount = this.calulateRate(totalBettingAmount.baccaratBettingAmount, store_member.getBaccaratRate());
 
+                        slot_store_rolling_amount = slot_store_rate_amount - slot_rolling_amount;
+                        baccarat_store_rolling_amount = baccarat_store_rate_amount - baccarat_rolling_amount;
                         float store_variableAmount = slot_store_rolling_amount + baccarat_store_rolling_amount;
 
                         System.out.println("IScheduleService==saveBettingSummary========store rolling data" +
@@ -282,6 +293,7 @@ public class ScheduleServiceImpl extends ServiceImpl<ScheduleMapper, BettingSumm
                             "reason:" + reason);
 
                     member.setCasinoMoney(member.getCasinoMoney() + variableAmount);
+                    member.setMileageAmount(member.getMileageAmount() + slot_rolling_amount + baccarat_rolling_amount);
 
 //                    Thomas 2022.04.27 add log money history of betting result
 //                    if(memberService.updateMemberHoldingMoney(
@@ -322,6 +334,8 @@ public class ScheduleServiceImpl extends ServiceImpl<ScheduleMapper, BettingSumm
                     bettingSummary.setStoreSeq(member.getStoreSeq() != null ? member.getStoreSeq() : "");
                     bettingSummary.setDistributorSeq(member.getDistributorSeq() != null ? member.getDistributorSeq() : "");
                     bettingSummary.setHeadquarterSeq(member.getSubHeadquarterSeq() != null ? member.getSubHeadquarterSeq() : "");
+                    bettingSummary.setSlotRollingAmount(slot_rolling_amount);
+                    bettingSummary.setBaccaratRollingAmount(baccarat_rolling_amount);
                     bettingSummary.setSlotStoreRollingAmount(slot_store_rolling_amount);
                     bettingSummary.setBaccaratStoreRollingAmount(baccarat_store_rolling_amount);
                     bettingSummary.setSlotDistributorRollingAmount(slot_distributor_rolling_amount);
